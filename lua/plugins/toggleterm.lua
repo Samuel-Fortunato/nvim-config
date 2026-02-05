ba3e418ea8ccb6ad475ef6c942f9e762819791c6
Samuel-Fortunato <samff2006@gmail.com>
2026-02-05 02:33:49 +0000

    added lazygit support

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,8 +1,36 @@
 return {
-   'akinsho/toggleterm.nvim',
-   event = "VeryLazy",
-   version = "*",
-   opts = {
-      open_mapping = [[<c-\>]],
-   }
+	'akinsho/toggleterm.nvim',
+	event = "VeryLazy",
+	version = "*",
+	keys = {
+		{ "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", desc = "LazyGit" },
+	},
+	opts = {
+		open_mapping = [[<c-\>]],
+		direction = 'float',
+	},
+	config = function(_, opts)
+	require("toggleterm").setup(opts)
+
+	-- Define the LazyGit terminal logic
+	local Terminal = require('toggleterm.terminal').Terminal
+	local lazygit = Terminal:new({
+		cmd = "lazygit",
+		dir = "git_dir",
+		direction = "float",
+		float_opts = {
+			border = "double",
+		},
+		-- Custom logic to handle focus and quitting
+		on_open = function(term)
+			vim.cmd("startinsert!")
+			vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+		end,
+	})
+
+	-- Define a global function so the keymap can find it
+	function _lazygit_toggle()
+		lazygit:toggle()
+	end
+end
 }
