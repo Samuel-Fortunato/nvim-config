e26160809eaf96f5c2937c40b1aa53d737cf72dc
Samuel-Fortunato <samff2006@gmail.com>
2026-02-05 00:48:05 +0000

    tweaked tabstop settings

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -2,6 +2,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 999
+vim.opt.showmode = false
 
 vim.opt.undofile = true
 
@@ -11,6 +12,7 @@ vim.opt.splitright = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.tabstop = 2
+vim.opt.tabstop = 8
+vim.opt.softtabstop = -1
+vim.opt.shiftwidth = 3
 vim.opt.expandtab = false
-vim.opt.shiftwidth = 0
