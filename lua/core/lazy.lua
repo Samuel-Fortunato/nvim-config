a36cbcfad14f7bb62d03031d2c1ece675ca71bf3
Samuel-Fortunato <samff2006@gmail.com>
2026-02-07 02:23:43 +0000

    LSP - added mason-lspconfig (and mason and lspconfig)

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "plugins" },
+		{ import = "plugins.lsp" },
 	},
 	defaults = { lazy = true },
 	checker = { enabled = true },
