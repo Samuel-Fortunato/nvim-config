d0aa89bae03f40be48ab32106c6c6a7fa3502019
Samuel-Fortunato <samff2006@gmail.com>
2026-02-07 16:16:30 +0000

    fix signcolumn conflict between gitsigns and vim.diagnostics

--- lua/lsp_config/diagnostics.lua
+++ lua/lsp_config/diagnostics.lua
@@ -1,7 +1,12 @@
 local severity = vim.diagnostic.severity
 
 vim.diagnostic.config({
+	virtual_lines = {
+		current_line = true
+	},
+	update_in_insert = true,
 	signs = {
+		priority = 9,
 		text = {
 			[severity.ERROR] = " ",
 			[severity.WARN] = " ",
