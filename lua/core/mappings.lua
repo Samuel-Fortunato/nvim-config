4313f7799788d8f5633be4eaf1b8bdc9953b6d06
Samuel-Fortunato <samff2006@gmail.com>
2026-02-08 02:13:37 +0000

    add tab switching keymaps

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "				
+vim.g.maplocalleader = " "
 
 -- <leader> keymaps
 
@@ -6,6 +7,8 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+vim.keymap.set("n", "<c-j>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<c-k>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
 -- Remove highlight
 vim.keymap.set("n", "<Esc>", vim.cmd.noh, { desc = "Clear search highlights" })
