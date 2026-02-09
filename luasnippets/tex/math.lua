48730cb6c2af50e8666d38eb9b2ab6414d72531a
Samuel-Fortunato <samff2006@gmail.com>
2026-02-09 19:49:33 +0000

    Add math snippets

--- luasnippets/tex/math.lua
+++ luasnippets/tex/math.lua
@@ -17,4 +17,54 @@ return {
 			{ d(1, get_visual), i(2) }
 		)
 	),
+	s({
+			trig = "int",
+			snippetType = "autosnippet",
+			condition = in_mathzone
+		},
+		fmta(
+			"\\int_{<>}^{<>} <>",
+			{ i(1), i(2), d(1, get_visual) }
+		)
+	),
+	s({
+			trig = "sum",
+			snippetType = "autosnippet",
+			condition = in_mathzone
+		},
+		fmta(
+			"\\sum_{<>}^{<>} <>",
+			{ i(1), i(2), d(1, get_visual) }
+		)
+	),
+	s({
+			trig = "sqrt",
+			snippetType = "autosnippet",
+			condition = in_mathzone
+		},
+		fmta(
+			"\\sqrt{<>}",
+			{ d(1, get_visual) }
+		)
+	),
+	s({
+			trig = "dd",
+			snippetType = "autosnippet",
+			condition = in_mathzone
+		},
+		fmta(
+			"\\frac{d<>}{d<>}",
+			{ d(1, get_visual), i(2) }
+		)
+	),
+	s({
+			trig = "pp",
+			snippetType = "autosnippet",
+			condition = in_mathzone
+		},
+		fmta(
+			"\\frac{\\partial<>}{\\partial<>}",
+			{ d(1, get_visual), i(2) }
+		)
+	),
 }
