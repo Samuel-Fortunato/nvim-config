27af17469dc27d90991b39de06d4510050ef4774
Samuel-Fortunato <samff2006@gmail.com>
2026-02-17 02:08:31 +0000

    enabled netrw as a fallback

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Disable netrw in favour of nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 require("core.set")
 require("core.mappings")
