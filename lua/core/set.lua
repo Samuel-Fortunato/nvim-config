2d0ea579c86077e2a8361b23012c4264f0e4ec3e
Samuel-Fortunato <samff2006@gmail.com>
2026-02-17 02:07:29 +0000

    general settings

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -18,3 +18,14 @@ vim.opt.softtabstop = 0
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = false
 vim.opt.breakindent = true
+
+vim.opt.inccommand = 'split'
+vim.opt.confirm = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = "Highlight text after yanking",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
