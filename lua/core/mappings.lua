1aa85fc85f58df0ec281d2257245baad84f426bf
Samuel-Fortunato <samff2006@gmail.com>
2026-03-14 19:43:59 +0000

    move arround keymaps

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -11,6 +11,12 @@ keymap("n", "<leader>S", "<Cmd>lua require('luasnip.loaders.from_lua').lazy_load
 keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 keymap("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+keymap("n", "<A-j>", "gj", { desc = "Move down (wrapped lines)" })
+keymap("n", "<A-k>", "gk", { desc = "Move up (wrapped lines)" })
+keymap("i", "<A-h>", "<Left>", { desc = "Move left (insert mode)" })
+keymap("i", "<A-j>", "<Down>", { desc = "Move down (insert mode)" })
+keymap("i", "<A-k>", "<Up>", { desc = "Move up (insert mode)" })
+keymap("i", "<A-l>", "<Right>", { desc = "Move right (insert mode)" })
 -- window navigation commands
 keymap("n", "<c-h>", "<c-w><c-h>", { desc = "Move to window in the left" })
 keymap("n", "<c-j>", "<c-w><c-j>", { desc = "Move to window below" })
